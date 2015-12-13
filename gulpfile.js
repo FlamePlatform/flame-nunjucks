dab3cc5dfb23398b5ed60a7293de3a02ed5e4eb9
Shavauhn Gabay <shavyg2@gmail.com>
2015-12-13 00:27:57 -0500

    batch:

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,18 @@ var print = require("gulp-print");
 
 gulp.task("build", function() {
   return gulp.src("src/**.js").
+  pipe(babel({
+    presets: ["es2015", "stage-0"],
+    plugins: ["transform-runtime"]
+  })).
+  pipe(print(function(file){
+    return "built "+file
+  })).
+  pipe(gulp.dest("dist"))
+})
+
+gulp.task("watch", function() {
+  gulp.src("src/**.js").
   pipe(watch("src/**.js")).
   pipe(babel({
     presets: ["es2015", "stage-0"],
